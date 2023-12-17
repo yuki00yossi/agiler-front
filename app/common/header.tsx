f04fbb6ae25e5aa5c5986e70408d6420819dab9e
yuki00yossi <yuki00yoshioka@gmail.com>
2023-12-17 21:21:44 +0900

    会員登録ページとログインページの作成

--- app/common/header.tsx
+++ app/common/header.tsx
@@ -28,7 +28,7 @@ export default function Header()
                     <Link href="/login">Login</Link>
                 </NavbarItem>
                 <NavbarItem>
-                <Button as={Link} color="primary" href="#" variant="flat">
+                <Button as={Link} color="primary" href="signup" variant="flat">
                     Sign Up
                     </Button>
                 </NavbarItem>
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -6,7 +6,7 @@ export default function Login()
     return (
         <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
             <div className="text-center">
-                <h2>ログイン</h2>
+                <h2>会員登録</h2>
             </div>
             <div className="mt-5 w-60 sm:mx-auto sm:w-full sm:max-w-sm p-5 border bg-white">
                 <form className="space-y-3" action="#" method="POST">
@@ -16,8 +16,17 @@ export default function Login()
                     <div>
                         <Input isRequired type="password" name="password" label="パスワード" labelPlacement={'inside'} description={'必須項目です'} />
                     </div>
+                    <div>
+                        <Input isRequired type="password" name="password_confirm" label="パスワード（確認用）" labelPlacement={'inside'} description={'必須項目です'} />
+                    </div>
+                    <div className="">
+                        <Input isRequired type="text" name="last_name" label="姓" labelPlacement={'inside'} description={'必須項目です'} />
+                    </div>
+                    <div className="">
+                        <Input isRequired type="text" name="first_name" label="名" labelPlacement={'inside'} description={'必須項目です'} />
+                    </div>
                     <div className="box mt-2">
-                        <Button color="primary" variant="ghost" className="w-full">ログイン</Button>
+                        <Button color="primary" variant="ghost" className="w-full">登録</Button>
                     </div>
                 </form>
             </div>
